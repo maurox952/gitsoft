07a220d4c8bfad6b1ead098091361117bcc0a1af
Mauricio <maurox952@gmail.com>
2020-12-06 13:54:20 -0500

    first commit

---
+++ BD/gitsoft.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-12-2020 a las 13:51:21
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gitsoft`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblcargo`
+--
+
+CREATE TABLE `tblcargo` (
+  `id_cargo` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblcliente`
+--
+
+CREATE TABLE `tblcliente` (
+  `id_cliente` varchar(30) NOT NULL,
+  `nombres` varchar(100) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `celular` varchar(20) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
+  `departamento` varchar(50) DEFAULT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `sexo` varchar(10) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblempleado`
+--
+
+CREATE TABLE `tblempleado` (
+  `id_empleado` varchar(30) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `celular` varchar(20) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `correo` varchar(100) NOT NULL,
+  `id_cargo` int(11) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
+  `departamento` varchar(50) DEFAULT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `sexo` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbllogin`
+--
+
+CREATE TABLE `tbllogin` (
+  `correo` varchar(100) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `id_empleado` varchar(30) DEFAULT NULL,
+  `id_cliente` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblnotificaciones`
+--
+
+CREATE TABLE `tblnotificaciones` (
+  `id_notificacion` int(11) NOT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `id_empleado` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblrol`
+--
+
+CREATE TABLE `tblrol` (
+  `id_rol` int(11) NOT NULL,
+  `descripcion` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tblcargo`
+--
+ALTER TABLE `tblcargo`
+  ADD PRIMARY KEY (`id_cargo`);
+
+--
+-- Indices de la tabla `tblcliente`
+--
+ALTER TABLE `tblcliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `fk_tblcliente_tblrol1_idx` (`id_rol`);
+
+--
+-- Indices de la tabla `tblempleado`
+--
+ALTER TABLE `tblempleado`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD UNIQUE KEY `correo_UNIQUE` (`correo`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id_empleado`),
+  ADD KEY `fk_tblempleado_tblcargo_idx` (`id_cargo`),
+  ADD KEY `fk_tblempleado_table11_idx` (`id_rol`);
+
+--
+-- Indices de la tabla `tbllogin`
+--
+ALTER TABLE `tbllogin`
+  ADD PRIMARY KEY (`correo`),
+  ADD KEY `fk_tbllogin_tblempleado1_idx` (`id_empleado`),
+  ADD KEY `fk_tbllogin_tblcliente1_idx` (`id_cliente`);
+
+--
+-- Indices de la tabla `tblnotificaciones`
+--
+ALTER TABLE `tblnotificaciones`
+  ADD PRIMARY KEY (`id_notificacion`),
+  ADD KEY `fk_tblnotificaciones_tblempleado1_idx` (`id_empleado`);
+
+--
+-- Indices de la tabla `tblrol`
+--
+ALTER TABLE `tblrol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tblcargo`
+--
+ALTER TABLE `tblcargo`
+  MODIFY `id_cargo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tblnotificaciones`
+--
+ALTER TABLE `tblnotificaciones`
+  MODIFY `id_notificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tblrol`
+--
+ALTER TABLE `tblrol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tblcliente`
+--
+ALTER TABLE `tblcliente`
+  ADD CONSTRAINT `fk_tblcliente_tblrol1` FOREIGN KEY (`id_rol`) REFERENCES `tblrol` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tblempleado`
+--
+ALTER TABLE `tblempleado`
+  ADD CONSTRAINT `fk_tblempleado_table11` FOREIGN KEY (`id_rol`) REFERENCES `tblrol` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tblempleado_tblcargo` FOREIGN KEY (`id_cargo`) REFERENCES `tblcargo` (`id_cargo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tbllogin`
+--
+ALTER TABLE `tbllogin`
+  ADD CONSTRAINT `fk_tbllogin_tblcliente1` FOREIGN KEY (`id_cliente`) REFERENCES `tblcliente` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tbllogin_tblempleado1` FOREIGN KEY (`id_empleado`) REFERENCES `tblempleado` (`id_empleado`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tblnotificaciones`
+--
+ALTER TABLE `tblnotificaciones`
+  ADD CONSTRAINT `fk_tblnotificaciones_tblempleado1` FOREIGN KEY (`id_empleado`) REFERENCES `tblempleado` (`id_empleado`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
