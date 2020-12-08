8b66a137b24adebb0840ebcd11636a481909257c
PC1\asfgm <sgiraldo776@misena.edu.co>
2020-12-08 16:20:39 -0500

    Estructura inicial del index

--- index.php
+++ index.php
@@ -4,8 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- JS BOOSTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
+    <style type="text/css">
+        .img-logo{
+            width: 100%;
+            height: 300px;
+            box-shadow: 0 0.5px 20px #888888;
+            background-image: url(img/icono.png);
+            background-size: cover;
+        }
+    </style>
 </head>
 <body>
-    <h1>estructira inicial</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="img/logo-text.png" alt="Logo" width="70" height="50"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="#">Incio</a>
+                    <a class="nav-link" href="#">Quiénes somos</a>
+                    <a class="nav-link" href="#">Nuestros servicios</a>
+                    <a class="nav-link" href="#">Clientes</a>
+                    <a class="nav-link" href="#">Contáctanos</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="img-logo">
+        
+    </div>
+    <section class="container text-center mt-5">
+        <h1>estructira inicial</h1>
+    </section>
+
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="my-3 my-lg-0 text-lg-center text-center">
+                    <a class="redes btn btn-social mx-3" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="redes btn btn-social mx-3" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="redes btn btn-social mx-3" href="#!"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
