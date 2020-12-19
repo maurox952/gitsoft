207d29899951c3dce7d14115cb91ee7ec21122ec
sam-oro <samiroroz@gmail.com>
2020-12-19 18:24:45 -0500

    Quienes somos

---
+++ vistas/quines/quienes_somos.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingresar</title>
+
+    <!--importacion boostrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Sofia&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="../../img/icono-pag.png">
+
+    <!-- Sweet alerts -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <link rel="stylesheet" href="../../css/estilos.css">
+</head>
+
+<body>
+
+    <section class="bg-quienes">
+        <div class="text-center">
+            <h2 class="titulo-cursiva">Quiénes somos</h2>
+        </div>
+    </section>
+
+
+    <section class="texto-quienes">
+        <div class="text-center ">
+            <h3>Su aliado estratégico</h3>
+            <h6>En Sistemas Integrados de Gestión</h6>
+        </div>
+        <div class="p-5">
+            <p>Como Consultores y Asesores integrales tenemos el conocimiento y la experiencia al diseñar, implementar, mantener y mejorar los Sistemas de Gestión de obligatorio cumplimiento bajo la normatividad vigente en Salud y Seguridad en el Trabajo
+                (SG-SST), Gestión Ambiental (SGA), Buenas Prácticas de Manufactura (BPM) y Fabricación (BPF), al igual de obtener y mantener certificaciones NTC ISO adheridas a estas normas y conforme al contexto de cada organización, apoyados de la metodología
+                en Gerencia de Proyectos PMI y las TIC´s permitiendo orientar los esfuerzos en alcanzar conceptos favorables de clientes o entes de control con el objetivo de minimizar costos y prevenir posibles sanciones a micro, pequeñas, medianas y
+                grandes empresas en general con prioridad en sectores de la cadena de alimentos. Con un poco más 2 años en el mercado y aunque las normas irrigan diversos sectores de la economía nos hemos especializado en el sector manufactura e industria,
+                específicamente del sector de la cadena de alimentos debido a que es en este campo donde hemos especializado nuestra capacidad de intervención, innovación y respuesta.</p>
+        </div>
+        <div class="text-center">
+            <button>Testimonios</button>
+        </div>
+    </section>
+
+    <section>
+        <br>
+        <h1>asd</h1>
+    </section>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
