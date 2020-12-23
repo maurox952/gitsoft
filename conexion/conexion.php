c3048815919c4917aec3ee54159c7b1198eb93f1
Mauricio <maurox952@gmail.com>
2020-12-23 16:00:09 -0500

    cosas

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,5 +1,5 @@
 <?php
 $conn = new mysqli('localhost','root','','gisot');
 //$URL = "http://localhost/gitsoft/"; //esta monda esta mala, el autista(dislexico) confundio las letras F
-$URL = "http://localhost/GIT/gitsoft/";
+$URL = "http://localhost/gitsoft/";
 ?>
