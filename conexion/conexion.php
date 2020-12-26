cce8b46e8a9bc0364f3551ef092c10960d673700
Mauricio <maurox952@gmail.com>
2020-12-26 15:22:14 -0500

    diseño contactenos

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,18 +1,10 @@
 <?php
-<<<<<<< HEAD
-// $conn = new mysqli('localhost','root','','gisot');
-// $URL = "http://localhost/gisot/";
+
+$conn = new mysqli('localhost','root','','gisot');
+$URL = "http://localhost/gitsoft/";
 
     //Samir
-$conn = new mysqli('localhost','root','','gisot','33065');
-$URL = "http://localhost:8080/gisot/";
-=======
-$conn = new mysqli('localhost','root','','gisot');
-//$URL = "http://localhost/gitsoft/"; //esta monda esta mala, el autista(dislexico) confundio las letras F
-<<<<<<< HEAD
-$URL = "http://localhost/gitsoft/";
-=======
-$URL = "http://localhost/GIT/gitsoft/";
->>>>>>> e28cd22ea44061421886fba0224b368919668c63
->>>>>>> dae096427aff3ac050c6b1a45cdcaf626a3719d4
+//$conn = new mysqli('localhost','root','','gisot','33065');
+//$URL = "http://localhost:8080/gisot/";
+
 ?>
